b96d7970b2ced69478a97789fef2cf70088d141f
Jannuel Dizon <jannuellaurodizon@gmail.com>
2024-07-22 14:15:36 +0800

    Learned how to create packages with ros2

---
+++ test_0/build/mycpp_package/CMakeFiles/mycpp_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycpp_node.dir/src/mycpp_node.cpp.o"
+  "CMakeFiles/mycpp_node.dir/src/mycpp_node.cpp.o.d"
+  "mycpp_node"
+  "mycpp_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mycpp_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
