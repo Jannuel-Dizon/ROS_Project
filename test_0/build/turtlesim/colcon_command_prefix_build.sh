08c815912ff8b5aef3eaf090305e02d401910a97
Jannuel Dizon <jannuellaurodizon@gmail.com>
2024-07-22 13:03:36 +0800

    First Commit

---
+++ test_0/build/turtlesim/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ylo-laptop/ROS_Practice/test_0/install/turtlesim_msgs/share/turtlesim_msgs/package.sh"
---
+++ test_0/build/turtlesim_msgs/turtlesim_msgs__py/CMakeFiles/turtlesim_msgs__py.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/_turtlesim_msgs_s.ep.rosidl_typesupport_c.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/_turtlesim_msgs_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/_turtlesim_msgs_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/action/__init__.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/action/_rotate_absolute.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/action/_rotate_absolute_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/msg/__init__.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/msg/_color.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/msg/_color_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/msg/_pose.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/msg/_pose_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/__init__.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_kill.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_kill_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_set_pen.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_set_pen_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_spawn.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_spawn_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_teleport_absolute.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_teleport_absolute_s.c"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_teleport_relative.py"
+  "/home/ylo-laptop/ROS_Practice/test_0/build/turtlesim_msgs/rosidl_generator_py/turtlesim_msgs/srv/_teleport_relative_s.c"
+  "CMakeFiles/turtlesim_msgs__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlesim_msgs__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
