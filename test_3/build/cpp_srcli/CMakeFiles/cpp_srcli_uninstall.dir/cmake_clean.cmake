7818383d6b1b80818b5a51d7d8e90c753683d2c0
Jannuel Dizon <jannuellaurodizon@gmail.com>
2024-07-23 10:47:28 +0800

    Basic Service Client code for ROS2 in cpp

---
+++ test_3/build/cpp_srcli/CMakeFiles/cpp_srcli_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_srcli_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cpp_srcli_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
