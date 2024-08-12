94f92534aecc58294943732840ff9119fd525c45
Jannuel Dizon <jannuellaurodizon@gmail.com>
2024-08-12 13:48:13 +0800

    Was finally able to build test5

---
+++ test_5/build/more_interfaces/CMakeFiles/publis_address_book.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/publis_address_book.dir/src/publish_address_book.cpp.o"
+  "CMakeFiles/publis_address_book.dir/src/publish_address_book.cpp.o.d"
+  "publis_address_book"
+  "publis_address_book.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publis_address_book.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_5/build/more_interfaces/more_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ylo-laptop/ROS_Projects/test_5/build/more_interfaces/more_interfaces__py
+# Build directory: /home/ylo-laptop/ROS_Projects/test_5/build/more_interfaces/more_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
